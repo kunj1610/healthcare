c306bb64bfb892248db0c8f70ea1cc54504d63a5
Kunj <U2404035@UEL.AC.UK>
2025-04-20 00:23:24 +0100

    updates

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,9 +1,14 @@
+// Import necessary dependencies from React
 import React from "react";
 
+// Import custom styles for the main container
 import "../styles/mainContainer.css";
 
+// MainContainer component that provides consistent layout wrapper
 function MainContainer(props) {
+  // Render main content area with children passed as props
   return <main>{props.children}</main>;
 }
 
+// Export the MainContainer component
 export default MainContainer;
