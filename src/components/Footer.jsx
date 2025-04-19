44faa6e6469e285b1b6ba64feeb1ef8ea272d06f
Kunj <U2404035@UEL.AC.UK>
2025-04-20 00:10:24 +0100

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,34 @@
+// Import React library for creating components
 import React from "react";
 
+// Import Bootstrap components for layout
 import { Container, Row, Col } from "react-bootstrap";
 
+// Import copyright icon from react-icons
 import { AiOutlineCopyrightCircle } from "react-icons/ai";
 
+// Import custom footer styles
 import "../styles/footer.css";
 
+// Footer component that displays copyright information
 function Footer() {
   return (
+    // Footer element wrapper
     <footer>
+      {/* Bootstrap container for footer content */}
       <Container className="footer-container">
+        {/* Single row */}
         <Row>
+          {/* Single column */}
           <Col>
             <div>
+              {/* Copyright text */}
               <span>Copyright</span>
+              {/* Copyright icon wrapper */}
               <span className="copyright-icon">
                 <AiOutlineCopyrightCircle />
               </span>
+              {/* Dynamic year display with rights reserved text */}
               <span>{new Date().getFullYear()} All rights reserved.</span>
         
             </div>
@@ -27,4 +39,5 @@ function Footer() {
   );
 }
 
+// Export Footer component as default
 export default Footer;
