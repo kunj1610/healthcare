197995bf0bf4388d97f0d88d4081803093779b40
Kunj <U2404035@UEL.AC.UK>
2025-04-20 00:35:34 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+// Import React and ReactDOM for rendering
 import React from "react";
 import ReactDOM from "react-dom";
 
+// Import CSS styles from Bootstrap and Ant Design
 import "bootstrap/dist/css/bootstrap.min.css";
 import "antd/dist/antd.css";
+// Import main App component
 import App from "./App";
 
+// Import and configure dotenv for environment variables
 import dotenv from "dotenv";
 dotenv.config();
 
+// Render the App component into the root DOM element
 ReactDOM.render(<App />, document.getElementById("root"));
