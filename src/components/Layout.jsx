a888c4b8d263b405a700549dcc649031ae017c46
Kunj <U2404035@UEL.AC.UK>
2025-04-20 00:24:21 +0100

    updates

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,13 @@
+// Import necessary dependencies from React
 import React from "react";
+// Import Header and Footer components
 import Header from "./Header";
 import Footer from "./Footer";
 
+// Import styled-components for styling
 import styled from "styled-components";
 
+// Create styled container component for layout
 const LayoutContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -11,14 +15,19 @@ const LayoutContainer = styled.div`
   height: 100vh;
 `;
 
+// Layout component that provides consistent page structure
 function Layout(props) {
   return (
     <LayoutContainer>
+      {/* Render header component */}
       <Header />
+      {/* Render child components passed as props */}
       {props.children}
+      {/* Render footer component */}
       <Footer />
     </LayoutContainer>
   );
 }
 
+// Export the Layout component
 export default Layout;
