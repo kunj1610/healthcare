2397922186e0cbefaa1b6478a23badf752fe440f
Kunj <U2404035@UEL.AC.UK>
2025-04-19 08:23:16 +0100

    updates

--- migrations/2_deploy_doctor_registry.js
+++ migrations/2_deploy_doctor_registry.js
@@ -1,5 +1,8 @@
+// Import the DoctorRegistry contract artifact
 const DoctorRegistry = artifacts.require("DoctorRegistry");
 
+// Export the deployment function
 module.exports = function (deployer) {
+  // Deploy the DoctorRegistry contract
   deployer.deploy(DoctorRegistry);
 };
