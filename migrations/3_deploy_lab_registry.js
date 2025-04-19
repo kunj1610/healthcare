60e91eabc475d108cd73abf68f9577cdba5455e2
Kunj <U2404035@UEL.AC.UK>
2025-04-19 08:24:12 +0100

    updates

--- migrations/3_deploy_lab_registry.js
+++ migrations/3_deploy_lab_registry.js
@@ -1,5 +1,8 @@
+// Import the LabRegistry contract artifact
 const LabRegistry = artifacts.require("LabRegistry");
 
+// Export the deployment function
 module.exports = function (deployer) {
+  // Deploy the LabRegistry contract
   deployer.deploy(LabRegistry);
 };
