a35b328206cd871c46fe08d9c3a7a04ad698f017
Kunj <U2404035@UEL.AC.UK>
2025-04-23 17:07:07 +0100

    feat: implement secure symmetric key derivation utility

--- src/cryptography/generatekeys.js
+++ src/cryptography/generatekeys.js
@@ -1,6 +1,6 @@
 import CryptoJS from "crypto-js";
 
-// ? Generate a symmetric Key from a password and key size in bits
+// ? Generate a symmetric Key from a password and key size in bits or retrurn
 function generateSymKey(password, keySize) {
   const salt = CryptoJS.lib.WordArray.random(128 / 8); // 16 bytes random salt
   const key = CryptoJS.PBKDF2(password, salt, { keySize: keySize / 32 });
